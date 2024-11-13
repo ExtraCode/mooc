b19e70813a4af9ab42d95b81e71c55d573dfdc66
Visiplus <visiplus@visiplus.fr>
2024-11-13 10:27:24 +0100

    Ajout du prix

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,9 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.scss';
 
+// Bootstrap 5.3.3
+import 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap';
+
+import './styles/app.scss';
